288542d22d2cc8bd1e5470c10d750ffc45324892
DanieleSpina92 <danielespina9292@gmail.com>
2022-11-30 11:12:25 +0100

    first commit
    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Counter, counter } from "./components/Counter";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+     <Counter/>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
